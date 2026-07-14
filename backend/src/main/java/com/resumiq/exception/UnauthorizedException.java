c67e28a986cb0e6e5e6470a7194f960908128c50
Gufran Ahmed <gufranahmed0921@gmail.com>
2026-07-15 03:41:29 +0530

    add unauthorized exception

---
+++ backend/src/main/java/com/resumiq/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.resumiq.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
