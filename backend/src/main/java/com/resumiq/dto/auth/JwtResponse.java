6cbd627ce33b47d6bed2311174fc5045c10b86e3
Gufran Ahmed <gufranahmed0921@gmail.com>
2026-07-16 15:04:04 +0530

    added JWT Response DTO

---
+++ backend/src/main/java/com/resumiq/dto/auth/JwtResponse.java
@@ -0,0 +1,30 @@
+package com.resumiq.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private String token;
+    private String tokenType;
+    private Long expiresIn;
+    private UserInfo user;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserInfo {
+        private Long id;
+        private String name;
+        private String email;
+        private String role;
+        private String department;
+    }
+}
